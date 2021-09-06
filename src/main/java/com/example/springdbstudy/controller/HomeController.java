693092902ef8218b049cf3ecf38aa32fde437db8
min <minimani0421@gmail.com>
2021-09-07 04:10:39 +0900

    lombok basic usage

--- src/main/java/com/example/springdbstudy/controller/HomeController.java
+++ src/main/java/com/example/springdbstudy/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.example.springdbstudy.controller;
 
+import com.example.springdbstudy.model.SalaryModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,21 +21,58 @@ public class HomeController {
 //        return "content/home";
 //    }
 
+//    @RequestMapping(value = "/home", method = RequestMethod.GET)
+//    public ModelAndView goHome(HttpServletRequest request) {
+//        ModelAndView mav = new ModelAndView();
+//        List<String> resultList = new ArrayList<String>();
+//
+//        resultList.add("AAA");
+//        resultList.add("BBB");
+//        resultList.add("CCC");
+//        resultList.add("DDD");
+//        resultList.add("FFF");
+//
+//        mav.addObject("resultList", resultList);
+//        mav.setViewName("content/home");
+//
+//        return mav;
+//    }
+
     @RequestMapping(value = "/home", method = RequestMethod.GET)
     public ModelAndView goHome(HttpServletRequest request) {
         ModelAndView mav = new ModelAndView();
-        List<String> resultList = new ArrayList<String>();
 
-        resultList.add("AAA");
-        resultList.add("BBB");
-        resultList.add("CCC");
-        resultList.add("DDD");
-        resultList.add("FFF");
+        // salary 객체 리스트 변수
+        List<SalaryModel> salaryList = new ArrayList<SalaryModel>();
 
-        mav.addObject("resultList", resultList);
+        // Salary model
+        SalaryModel salaryModel;
+
+        // 첫번째 데이터
+        salaryModel = SalaryModel.builder()
+                .id(1)
+                .name("AAA")
+                .email("AAA@example.com").build();
+        salaryList.add(salaryModel);
+
+        // 두번째 데이터
+        salaryModel = SalaryModel.builder()
+                .id(2)
+                .name("BBB")
+                .email("BBB@example.com").build();
+        salaryList.add(salaryModel);
+
+        // 첫번째 데이터
+        salaryModel = SalaryModel.builder()
+                .id(3)
+                .name("CCC")
+                .email("CCC@example.com").build();
+        salaryList.add(salaryModel);
+
+        mav.addObject("salaryList", salaryList);
         mav.setViewName("content/home");
 
-        return mav;
+        return  mav;
     }
 
 }
--- src/main/java/com/example/springdbstudy/model/SalaryModel.java
+++ src/main/java/com/example/springdbstudy/model/SalaryModel.java
@@ -1,10 +1,13 @@
 package com.example.springdbstudy.model;
 
+import lombok.Builder;
 import lombok.Data;
+import lombok.NonNull;
 
 @Data
+@Builder
 public class SalaryModel {
     private int id;
-    private String name;
+    @NonNull @Builder.Default private String name = "NULL Name"; // null 값을 허용하지 않는다.
     private String email;
 }
